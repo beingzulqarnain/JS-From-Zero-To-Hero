4a171e9291789d729e875728f1231528315914ab
Zul Qarnain Afridi | MERN Stack Developer <beingzulqarnain@gmail.com>
2024-10-06 02:10:51 +0500

    starting callback topic

--- CallBack_Promise_Async In JS/index.js
+++ CallBack_Promise_Async In JS/index.js
@@ -1,1 +1,20 @@
-// Promise 
+// Sychronization Process
+// step-By-step Execution run likewise code1,code2,code3 like that it can be sycroniztion process 
+console.log("One");
+console.log("two");
+console.log("Three");
+console.log("Four");
+//Asychronization Process
+// step-By-step Execution run likewise code1,code2,code3 but not wait all the excution instead the time required to excute the function,it can be Asycroniztion process 
+setTimeout(()=> console.log("Hello Friends...") ,2000)
+console.log("Five");
+console.log("Six");
+/*
+==============================================================
+                  Callback Function
+     Callback is a function passed as an argument to another function.
+==============================================================
+*/
+✅
+
+
